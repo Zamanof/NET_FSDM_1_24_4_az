06e52c51b1cd38e4225a3eecff8a001ca40f89ce
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-12-20 10:06:32 +0400

    Delegates intro

---
+++ NET 08. Delegates/Program.cs
@@ -0,0 +1,111 @@
+﻿// Delegates
+// delegate retrun_datatype delegate_name(parameters)
+// public delegate int MyIntDelegate(int numb1, int numb2)
+
+#region delegate
+
+//double numb1 = default, numb2 = default;
+//Console.WriteLine("Enter first number: ");
+//double.TryParse(Console.ReadLine(), out numb1);
+//Console.WriteLine("Enter second number: ");
+//double.TryParse(Console.ReadLine(), out numb2);
+
+//char operation = default;
+//Console.WriteLine("Choice operation: +, -, *, /");
+//char.TryParse(Console.ReadLine(), out operation);
+
+//CalculatorDelegate calculator = null;
+
+//switch ((operators)operation)
+//{
+//    case operators.ADD:
+//        calculator = Add;
+//        break;
+//    case operators.SUBRTACT:
+//        calculator = Subtract;
+//        break;
+//    case operators.MULTIPLE:
+//        calculator = Multiple;
+//        break;
+//    case operators.DIVIDE:
+//        calculator = Divide;
+//        break;
+//    default:
+//        Console.WriteLine("Incorrect operation!!!");
+//        break;
+//}
+////if (calculator is not null)
+////{
+////    Console.WriteLine(calculator(numb1, numb2));
+////}
+
+//if (calculator is not null)
+//{
+//    Console.WriteLine(calculator.Invoke(numb1, numb2));
+
+//}
+
+//double Add(double left, double right)
+//{
+//    return left + right;
+//}
+//double Subtract(double left, double right)
+//{
+//    return left - right;
+//}
+//double Multiple(double left, double right)
+//{
+//    return left * right;
+//}
+
+//double Divide(double left, double right)
+//{
+//    if (right != 0) return left / right;
+//    throw new DivideByZeroException();
+//}
+
+//public delegate double CalculatorDelegate(double left, double right);
+//enum operators { ADD = '+', SUBRTACT = '-', MULTIPLE = '*', DIVIDE = '/' }
+#endregion
+
+#region Multicast delegates
+CalculatorDelegate calculators = Add;
+calculators += Subtract;
+calculators += Multiple;
+calculators += Divide;
+
+double number1 = 56, number2 = 32;
+
+//Console.WriteLine(calculators(number1, number2));
+
+//foreach( CalculatorDelegate calculator in calculators.GetInvocationList())
+//{
+//    Console.WriteLine(calculator(number1, number2));
+//}
+
+var delegateList = calculators.GetInvocationList();
+Console.WriteLine(delegateList[0].Method);
+Console.WriteLine(delegateList[1].Method);
+Console.WriteLine(delegateList[3].DynamicInvoke(25, 22));
+
+double Add(double left, double right)
+{
+    return left + right;
+}
+double Subtract(double left, double right)
+{
+    return left - right;
+}
+double Multiple(double left, double right)
+{
+    return left * right;
+}
+
+double Divide(double left, double right)
+{
+    if (right != 0) return left / right;
+    throw new DivideByZeroException();
+}
+
+public delegate double CalculatorDelegate(double left, double right);
+#endregion
