561d4b061b532738de6e195a23f9c8335130eb51
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2024-12-27 12:00:29 +0400

    LINQ methods

---
+++ NET 11. LINQ methods/Student.cs
@@ -0,0 +1,27 @@
+﻿// LINQ methods
+// https://learn.microsoft.com/en-us/dotnet/csharp/linq/get-started/introduction-to-linq-queries
+class Student
+{
+    public uint Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public byte Age { get; set; }
+    public uint GroupId { get; set; }
+    static uint count = 1;
+
+    public Student()
+    {
+        Id = count++;
+    }
+    public override string ToString()
+    {
+        return $"""
+            Id:{Id}
+            FirstName:      {FirstName}
+            LastName:       {LastName}
+            Age:            {Age}
+            GroupId         {GroupId}
+
+            """;
+    }
+}
